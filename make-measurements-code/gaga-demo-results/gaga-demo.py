3c31f168efcee4bc36de3ef3a8c882ad16ba1f9c
Alison <awon8465@uni.sydney.edu.au>
2019-06-13 17:31:53 +1000

    Demo using gaga

--- make-measurements-code/gaga-demo-results/gaga-demo.py
+++ make-measurements-code/gaga-demo-results/gaga-demo.py
@@ -10,48 +10,44 @@ gene_definition = {"dust_mass": (1e-7, 6e-7),
 
 def evaluate(individual):
 
-    WAVELENGTH = 0.75 # wavelength of observation in microns
-
-    # location where intermediate mcfost files are created
+    WAVELENGTH = 0.75 # microns
     MCFOST_path = "../mcfost-workspace/"
 
-    # name of the python file containing the Default_parameters class
     default_params = "default_parameters_star"
 
-    # path to the VAMPIRES data
     VAMP_path = ""
     VAMP_data = "diffdata_RLeo_03_20170313_750-50_18holeNudged_0_0.idlvar"
     VAMP_data_info = "cubeinfoMar2017.idlvar"
 
     VAMP_data = VAMPIRES_data.VAMPIRES_data(VAMP_path + VAMP_data, VAMP_path + VAMP_data_info)
 
-    # remove bias from the observed data i.e. move the average to 1
+    # remove out bias from the observed data i.e. move the average to 1
+
     VAMP_data.vhvv -= np.mean(VAMP_data.vhvv) - 1
     VAMP_data.vhvvu -= np.mean(VAMP_data.vhvvu) - 1
 
-    # unpack the genes and use them to define the parameters for the model
     dm = individual.genes["dust_mass"]
     Rin = individual.genes["inner_radius"]
     r = individual.genes["radius"]
     Rout = individual.genes["outer_radius"]
 
     free_params = {"dust_mass": dm, "Rin": Rin, "radius":r, "Rout":Rout}
-
-    # run mcfost
     s = star.Star(free_params, default_params, WAVELENGTH, MCFOST_path, VAMP_data, load=False, verbose=False)
 
-    # calculate the reduced chi^2 error and use it as the fitness score
     s.calculate_reduced_chi2error()
-    individual.fitness_score = s.reduced_chi2err
 
-    # Optional: storing some useful data
     individual.data["data_Q"] = s.data_Q
     individual.data["data_U"] = s.data_U
     individual.data["reduced_chi2err_Q"] = s.reduced_chi2err_Q
     individual.data["reduced_chi2err_U"] = s.reduced_chi2err_U
     individual.data["reduced_chi2err"] = s.reduced_chi2err
 
-# NOTE: This GA has not been tuned
+    # We want to enforce the standard deviation
+
+    llambda = 10000 # -> if Q has a standard deviation of ~0, this should map to a fitness of ~150
+
+    individual.fitness_score = s.reduced_chi2err
+
 sim = ga.ga(gene_definition,
             evaluate,
             mutate = 0.6,
@@ -59,7 +55,7 @@ sim = ga.ga(gene_definition,
             mutate_crossover = True,
             population_size = 25,
             verbose = True,
-            results_folder = "gaga-demo-results")
+            results_folder = "init-vamp-fit")
 
 sim.run_simulation()
 
